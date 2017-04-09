7a1c453583ef2536226994f5f2bbe8fa5eec417e
Oliver Beck <o.oliver.beck@googlemail.com>
2017-04-09 12:46:29 +0000

    Added comments

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,18 +1,30 @@
 class ContactsController < ApplicationController
+   
+   # GET request to /contact-us
+   #Show new contact form
    def new
       @contact = Contact.new
    end
    
+   # POST request /contacts
    def create
+      # Mass assignment of form fields into Contact object
       @contact = Contact.new(contact_params)
+      # Save the Contact object to the database
       if @contact.save
+         # Store form fields via parameters into variables
          name = params[:contact][:name]
          email = params[:contact][:email]
          body = params[:contact][:comments]
+         # Put variables into Contact Mail method and send mail
          ContactMailer.contact_email(name, email, body).deliver
+         # Store success message in flash hash
          flash[:success] = "Message sent."
+         #redirect to the new action
          redirect_to new_contact_path
       else
+         # If Contact object doesn't save, store errors in flash hash
+         # and redirect to the new action
          flash[:danger] = @contact.errors.full_messages.join(", ")
          redirect_to new_contact_path
       end
