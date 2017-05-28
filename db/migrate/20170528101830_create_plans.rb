352f50135853153fec8b273b6699a568d82d1272
Oliver Beck <o.oliver.beck@googlemail.com>
2017-05-28 12:11:40 +0000

    added sign up links to home page

--- db/migrate/20170528101830_create_plans.rb
+++ db/migrate/20170528101830_create_plans.rb
@@ -3,7 +3,7 @@ class CreatePlans < ActiveRecord::Migration[5.0]
     create_table :plans do |t|
       t.string :name
       t.decimal :price
-      t.timestamps
+      t.timestamps 
     end
   end
 end
